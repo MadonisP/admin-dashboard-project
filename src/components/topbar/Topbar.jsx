29d2c235d4b2e273cf1f6e4cddfb574f809cba1c
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-06-24 00:39:03 +0300

    admin dashboard project
    react admin dashboard

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -12,17 +12,17 @@ const Topbar = () => {
                 <div className="topRight">
                     <div className="topbarIconContainer">
                         <NotificationsNoneOutlined />
-                        <span className="topIconBag">2</span>
+                        <span className="topIconBadge">2</span>
                     </div>
                     <div className="topbarIconContainer">
                         <Language />
-                        <span className="topIconBag">2</span>
+                        <span className="topIconBadge">2</span>
                     </div>
                     <div className="topbarIconContainer">
                         <Settings />
-                        <span className="topIconBag">2</span>
+                        <span className="topIconBadge">2</span>
                     </div>
-                   
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
                 </div>
             </div>
         </div>
