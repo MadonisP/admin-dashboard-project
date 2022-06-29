949aafd6c2769682a080fda2723059b71e861e04
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-06-29 16:08:01 +0300

    admin dashboard project
    admin dashboard project with react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 function App() {
   return (
@@ -12,9 +13,9 @@ function App() {
       <div className="container">
         <Sidebar />
         <Routes>
-          <Route exact path='/' element={ <Home />}/>
-          <Route path='/users' element={ <UserList />} />
-           
+          <Route exact path='/' element={<Home />} />
+          <Route path='/users' element={<UserList />} />
+          <Route path='/user/:userId' element={<User/>}/> 
         </Routes>
       </div>
     </Router>
